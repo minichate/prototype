3dfde2c93b3669943d24ea2fc56163badfc6e3fb
jdalton <john.david.dalton@gmail.com>
2008-05-16 10:36:35 -0500

    LH#9 part1: Ensure Element#getDimensions works with display:none ancestors

--- src/dom.js
+++ src/dom.js
@@ -441,25 +441,34 @@ Element.Methods = {
   
   getDimensions: function(element) {
     element = $(element);
-    var display = element.getStyle('display');
-    if (display != 'none' && display != null) // Safari bug
-      return {width: element.offsetWidth, height: element.offsetHeight};
-    
-    // All *Width and *Height properties give 0 on elements with display none,
-    // so enable the element temporarily
-    var els = element.style;
-    var originalVisibility = els.visibility;
-    var originalPosition = els.position;
-    var originalDisplay = els.display;
-    els.visibility = 'hidden';
-    els.position = 'absolute';
-    els.display = 'block';
-    var originalWidth = element.clientWidth;
-    var originalHeight = element.clientHeight;
-    els.display = originalDisplay;
-    els.position = originalPosition;
-    els.visibility = originalVisibility;
-    return {width: originalWidth, height: originalHeight};    
+    var dimensions = {width: element.clientWidth, height: element.clientHeight};
+   
+    // *Width or *Height properties can return 0 on elements with display none,
+    // or when ancestors have display none, so enable those temporarily
+    if (dimensions.width == 0 || dimensions.height == 0) {
+      var check = element.ancestors(),
+      restore = [], styles = [];
+      check.push(element);
+      
+      check.each(function(c) {
+        if (c != element && c.visible()) return;
+        restore.push(c);
+        styles.push({
+          display: c.getStyle('display'),
+          position: c.getStyle('position'),
+          visibility: c.getStyle('visibility')
+        });
+        c.setStyle('display:block;visibility:visible;position:absolute;');
+      });
+      
+      // get the dimensions
+      dimensions = {width: element.clientWidth, height: element.clientHeight};
+      
+      // restore the styles
+      for (var i = 0, length = restore.length; i < length; i++)
+        restore[i].setStyle(styles[i]);
+    }
+    return dimensions;
   },
   
   makePositioned: function(element) {
