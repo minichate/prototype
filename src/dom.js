f0a5cd51080d021aab0090d5a0130df0af63f82d
jdalton <john.david.dalton@gmail.com>
2008-05-16 09:06:36 -0500

    Fix #10486 Element#viewportOffset stopped working in Opera 9.5

--- src/dom.js
+++ src/dom.js
@@ -614,7 +614,7 @@ Element.Methods = {
 
     element = forElement;
     do {
-      if (!Prototype.Browser.Opera || element.tagName.toUpperCase() == 'BODY') {
+      if (!Prototype.Browser.Opera || element.tagName.toUpperCase() == 'HTML') {
         valueT -= element.scrollTop  || 0;
         valueL -= element.scrollLeft || 0;
       }
