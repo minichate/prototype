01a2b30035552246c05be84c2e5500e29a906cf5
jdalton <john.david.dalton@gmail.com>
2008-06-24 16:27:32 -0500

    Make Prototype.BrowserFeatures use type checking and correct a browser sniff regression bug introduced a couple of days ago

--- src/prototype.js
+++ src/prototype.js
@@ -4,21 +4,22 @@ var Prototype = {
   Version: '<%= PROTOTYPE_VERSION %>',
   
   Browser: {
-    IE:     window.attachEvent && navigator.userAgent.indexOf('Opera') == -1,
+    IE:     !!(window.attachEvent && navigator.userAgent.indexOf('Opera') === -1),
     Opera:  navigator.userAgent.indexOf('Opera') > -1,
     WebKit: navigator.userAgent.indexOf('AppleWebKit/') > -1,
-    Gecko:  navigator.userAgent.indexOf('Gecko') > -1 && navigator.userAgent.indexOf('KHTML') == -1,
+    Gecko:  navigator.userAgent.indexOf('Gecko') > -1 && navigator.userAgent.indexOf('KHTML') === -1,
     MobileSafari: !!navigator.userAgent.match(/Apple.*Mobile.*Safari/)
   },
 
   BrowserFeatures: {
-    XPath: !!document.evaluate,
-    SelectorsAPI: !!document.querySelector,
-    ElementExtensions: !!window.HTMLElement,
-    SpecificElementExtensions: 
-      document.createElement('div')['__proto__'] &&
-      document.createElement('div')['__proto__'] !== 
-        document.createElement('form')['__proto__']
+    XPath: typeof document.evaluate === 'function',
+    SelectorsAPI: typeof document.querySelector === 'function',
+    ElementExtensions: typeof window.HTMLElement === 'function' || typeof window.HTMLElement === 'object',
+    SpecificElementExtensions: (function() {
+      var proto1 = document.createElement('div')['__proto__'], proto2 = document.createElement('form')['__proto__'];
+      return proto1 && proto2 && typeof proto1.appendChild === 'function' &&
+        typeof proto2.appendChild === 'function' && proto1 !== proto2;
+    })()
   },
 
   ScriptFragment: '<script[^>]*>([^\\x00]*?)<\/script>',
